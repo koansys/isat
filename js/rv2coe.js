8d6f17dbfd1a053c4b5860b4e4d286aed030bddb
Chris Shenton <chris@koansys.com>
2012-02-08 15:42:44 -0500

    Split MATLAB-emulating math utility functions into own file

--- js/rv2coe.js
+++ js/rv2coe.js
@@ -59,44 +59,6 @@
 // [p,a,ecc,incl,omega,argp,nu,m,arglat,truelon,lonper ] = rv2coe (r,v,mu);
 // ------------------------------------------------------------------------------
 
-function sign(val) {
-    if (val < 0) {
-        return -1;
-    }
-    if (val > 0) {
-        return 1;
-    }
-    return 0;
-}
-
-function cross(v1, v2) {
-    // Return 3d cross product vector from two 3d vectors.
-    // http://knol.google.com/k/dot-product-cross-product-in-3d
-    // http://rosettacode.org/wiki/Vector_products
-    // Our vectors are represented as lists, for historical compatibility.
-    var x1 = v1[0],
-        y1 = v1[1],
-        z1 = v1[2],
-        x2 = v2[0],
-        y2 = v2[1],
-        z2 = v2[2];
-    return [y1 * z2 - y2 * z1, z1 * x2 - z2 * x1, x1 * y2 - x2 * y1];
-}
-
-function dot(a, b) {
-    // From: http://c2.com/cgi/wiki?DotProductInManyProgrammingLanguages
-    var n = 0,
-    lim = Math.min(a.length, b.length),
-    i;
-
-    for (i = 0; i < lim; i += 1) {
-        n += a[i] * b[i];
-    }
-    return n;
-}
-//assert(dot([1, 2, 3, 4, 5], [6, 7, 8, 9, 10]) == 130)
-
-
 function rv2coe(r, v, mu) {
     //TODO: get contants from constmath, constastro
     //constmath;
