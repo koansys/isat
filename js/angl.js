cc21aba37c378d92b1b475d650465bd7fcf1922d
Chris Shenton <chris@koansys.com>
2012-02-08 15:41:56 -0500

    Test angl, find and fix bug (didn't return the calculated value)

--- js/angl.js
+++ js/angl.js
@@ -1,26 +1,29 @@
-
-// %  this function calculates the angle between two vectors.  the output is
-// %    set to 999999.1 to indicate an undefined value.  be sure to check for
-// %    this at the output phase.
-// %
-// % [theta] = angl ( vec1,vec2 );
-// % ----------------------------------------------------------------------------- }
+/*global mag: true, dot: true, sign: true
+ */
+// this function calculates the angle between two vectors.  the output is
+// set to 999999.1 to indicate an undefined value.  be sure to check for
+// this at the output phase.
+//
+// [theta] = angl ( vec1,vec2 );
+// -----------------------------------------------------------------------------
 
 function angl(vec1, vec2) {
     var SMALL     = 0.00000001,
         UNDEFINED = 999999.1,
         magv1     = mag(vec1),
         magv2     = mag(vec2),
-        temp;
+        temp,
+        theta;
 
-    if (magv1*magv2 > SMALL^2) {
-        temp = dot(vec1,vec2) / (magv1*magv2);
+    if (magv1 * magv2 > SMALL * SMALL) {
+        temp = dot(vec1, vec2) / (magv1 * magv2);
         if (Math.abs(temp) > 1.0) {
             temp = sign(temp) * 1.0;
         }
-        theta= acos( temp );
+        theta = Math.acos(temp);
     }
     else {
-        theta= UNDEFINED;
+        theta = UNDEFINED;
     }
+    return theta;
 }
