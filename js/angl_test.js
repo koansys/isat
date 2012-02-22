eaec27c01c3e998160e0d80079e3861aefcf11d7
Chris Shenton <chris@koansys.com>
2012-02-22 15:29:09 -0500

    Unglobalize the TOLERANCE

--- js/angl_test.js
+++ js/angl_test.js
@@ -5,9 +5,9 @@
 
 module('angl');
 
-TOLERANCE = 1e-15;
-
 test('angl samples', function () {
+    var TOLERANCE = 1e-15;
+
     equal(angl([0, 0, 1], [0, 0, 1]), 0.00000000000000e+00, "same vectors");
     ok(Math.abs(angl([0, 0, 1], [0, 1, 1]) - 7.85398163397448e-01) < TOLERANCE, "two dimensions");
     ok(Math.abs(angl([0, 0, 1], [1, 1, 1]) - 9.55316618124509e-01) < TOLERANCE, "three dimensions");
