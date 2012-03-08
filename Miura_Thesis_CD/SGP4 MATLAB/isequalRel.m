9292efd2784ed4130fcfd29c1be2bb9c0255bf74
Chris Shenton <chris@koansys.com>
2012-03-08 11:54:34 -0500

    Comment to indicate source of algorithm

--- Miura_Thesis_CD/SGP4 MATLAB/isequalRel.m
+++ Miura_Thesis_CD/SGP4 MATLAB/isequalRel.m
@@ -1,3 +1,6 @@
+%% tolerance test implementation from
+%% http://stackoverflow.com/questions/2202641/how-do-i-compare-all-elements-of-two-arrays
+
 function yesno = isequalRel(x,y,tol);
   yesno = ( abs(x-y) <= ( tol*max(abs(x),abs(y)) + eps) );
   if (!  yesno)
