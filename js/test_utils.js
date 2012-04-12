5d302859a4bb254afcc3713316abc8d0e1509597
Chris Shenton <chris@koansys.com>
2012-04-12 17:47:04 -0400

    Throw helpful information -- if uncommented

--- js/test_utils.js
+++ js/test_utils.js
@@ -1,6 +1,14 @@
+/*global ok: true
+ */
+
 function isequalRel(x, y, tol) {
     var EPS = Math.pow(2, -52); // IEEE 754 double precision epsilon
-    return (Math.abs(x - y) <= (tol * Math.max(Math.abs(x), Math.abs(y)) + EPS));
+    if (Math.abs(x - y) <= (tol * Math.max(Math.abs(x), Math.abs(y)) + EPS)) {
+        return true;
+    }
+    // Too bad this stops QUnit test, but is handy for finding want/got values
+    //throw new Error("isEqualRel x=" + x + " y=" + y + "  tol=" + tol);
+    return false;
 }
 
 function assert(state, message) {
