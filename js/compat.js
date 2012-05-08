fa180f24480255fc71cc69cec8ff9aca800d9f80
Chris Shenton <chris@koansys.com>
2012-05-08 15:07:25 -0400

    Tolerate blank lines in TLE data, trim leading/trailing from all TLE lines

--- js/compat.js
+++ js/compat.js
@@ -5,6 +5,12 @@
 String.prototype.trim = function () {
     return this.replace(/^\s+|\s+$/g, "");
 };
+String.prototype.ltrim = function () {
+    return this.replace(/^\s+/, "");
+};
+String.prototype.rtrim = function () {
+    return this.replace(/\s+$/, "");
+};
 
 // MATLAB fake compatibility output functions
 // These rather assume the output elements are <pre/>
