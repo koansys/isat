df48321dc2f2ef130791655003bbd17894c36d56
Chris Shenton <chris@koansys.com>
2012-09-20 17:30:11 -0400

    Add ISS orbit track! Geolocate for initial view.
    Track is not in correct coordinate ref frame (ECI TEME vs ECF)
    Track is probably not in correct time; need to rerun calcs based on
    current Julian date..

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*global document, Cesium, Image*/
+/*global document, Cesium, Image, issPoints, console, navigator*/
 (function () {
     "use strict";
     var canvas = document.getElementById('glCanvas');
@@ -16,105 +16,64 @@
 
     var cb = new Cesium.CentralBody(ellipsoid);
     // How do we tell if we can't get Bing, and substitute flat map?
-    cb.dayTileProvider      = bing;
-    // is this map right? seamless with the night view? 
     //cb.dayTileProvider      = new Cesium.SingleTileProvider('Images/NE2_50M_SR_W_4096.jpg');
-
+    cb.dayTileProvider      = bing;
     cb.nightImageSource     = 'Images/land_ocean_ice_lights_2048.jpg';
-    cb.specularMapSource    = 'Images/earthspec1k.jpg';
-//    cb.cloudsMapSource      = 'Images/earthcloudmaptrans.jpg';
     cb.bumpMapSource        = 'Images/earthbump1k.jpg';
     cb.showSkyAtmosphere    = true;
-//    cb.showGroundAtmosphere = true;
 
     primitives.setCentralBody(cb);
 
     scene.getCamera().getControllers().addCentralBody();
     scene.getCamera().getControllers().get(0).spindleController.constrainedAxis = Cesium.Cartesian3.UNIT_Z;
-
     scene.getCamera().lookAt({
-        eye : new Cesium.Cartesian3(2203128.2853925996, -7504680.128731707, 5615591.201449535),
-        target : Cesium.Cartesian3.ZERO,
-        up : new Cesium.Cartesian3(-0.1642824655609347, 0.5596076102188919, 0.8123118822806428)
+        eye : new Cesium.Cartesian3(4000000.0, -15000000.0,  10000000.0),
+        up : new Cesium.Cartesian3(-0.1642824655609347, 0.5596076102188919, 0.8123118822806428),
+        target : Cesium.Cartesian3.ZERO
     });
 
-    function addBillboardsInReferenceframe(scene, ellipsoid) {
+    function addIssPointsInReferenceframe(scene, ellipsoid) {
+        var theIssPoints = issPoints();
         var image = new Image();
-        image.onload = function () {
-            var billboards = new Cesium.BillboardCollection();
-            var textureAtlas = scene.getContext().createTextureAtlas({image: image});
-            billboards.setTextureAtlas(textureAtlas);
-            var center = ellipsoid.cartographicToCartesian(
-                Cesium.Cartographic.fromDegrees(-77.141209,38.885580)); // WBG Lon,Lat
-            billboards.modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(center);
-            //billboards.add({ imageIndex : 0, position : new Cesium.Cartesian3(0.0, 0.0, 0.0) }); // center
-            billboards.add({ imageIndex : 0, position : new Cesium.Cartesian3(1000000.0, 0.0, 0.0) }); // east
-            billboards.add({ imageIndex : 0, position : new Cesium.Cartesian3(0.0, 1000000.0, 0.0) }); // north
-            billboards.add({ imageIndex : 0, position : new Cesium.Cartesian3(0.0, 0.0, 1000000.0) }); // up
-            scene.getPrimitives().add(billboards);
-        };
         image.src = 'Images/Satellite.png';
-    }
-    addBillboardsInReferenceframe(scene, ellipsoid);
-
-    function addWBGBillboardsInReferenceframe(scene, ellipsoid) {
-        var image = new Image();
         image.onload = function () {
             var billboards = new Cesium.BillboardCollection();
             var textureAtlas = scene.getContext().createTextureAtlas({image: image});
+            var ip;
             billboards.setTextureAtlas(textureAtlas);
-            var center = ellipsoid.cartographicToCartesian(
-                Cesium.Cartographic.fromDegrees(-77.141209,38.885580)); // WBG Lon,Lat
-            billboards.modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(center);
-            billboards.add({ imageIndex : 0, position : new Cesium.Cartesian3(0.0, 0.0, 0.0) }); // center
+            for (ip = 0; ip < theIssPoints.length; ip++) {
+                billboards.add({imageIndex: 0,
+                                position: new Cesium.Cartesian3(theIssPoints[ip].x, theIssPoints[ip].y, theIssPoints[ip].z)});
+            }
             scene.getPrimitives().add(billboards);
-        };
-        image.src = 'Images/icon_beer.gif';
+        }
     }
-    addWBGBillboardsInReferenceframe(scene, ellipsoid);
+    addIssPointsInReferenceframe(scene, ellipsoid);
 
-    // SAC in South Africa
-    function addSACInReferenceframe(scene, ellipsoid) {
-        var image = new Image();
-        image.onload = function () {
-            var billboards = new Cesium.BillboardCollection();
-            var textureAtlas = scene.getContext().createTextureAtlas({image: image});
-            billboards.setTextureAtlas(textureAtlas);
-            var center = ellipsoid.cartographicToCartesian(
-                Cesium.Cartographic.fromDegrees(27,-25)); // SAC Lon,Lat
-            billboards.modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(center);
-            billboards.add({ imageIndex : 0, position : new Cesium.Cartesian3(0.0, 0.0, 0.0) }); // center
-            scene.getPrimitives().add(billboards);
-        };
-        image.src = 'Images/facility.gif';
+    function viewByGeolocation() {
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(show_geo);
+        }
+        else {
+            console.log('I can not haz geo :-(');
+        }
+        function show_geo(position) {
+            var target = ellipsoid.cartographicToCartesian(
+                Cesium.Cartographic.fromDegrees(position.coords.longitude, position.coords.latitude));
+            var eye    = ellipsoid.cartographicToCartesian(
+                Cesium.Cartographic.fromDegrees(position.coords.longitude, position.coords.latitude, 1e7));
+            // console.log('lat=' + position.coords.latitude + ' lon=' + position.coords.longitude);
+            // console.log('cartesian=' + target);
+            scene.getCamera().lookAt({
+                up : new Cesium.Cartesian3(0,0,1),
+                eye : eye,
+                target : target
+            });
+
+        }
     }
-    addSACInReferenceframe(scene, ellipsoid);
+    viewByGeolocation();
 
-
-    function addLabelsInReferenceFrame(scene, ellipsoid) {
-        var center = ellipsoid.cartographicToCartesian(
-            Cesium.Cartographic.fromDegrees(-77.141209,38.885580)); // WBG Lon,Lat
-        var labels = new Cesium.LabelCollection(undefined);
-        labels.modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(center);
-        labels.add({
-            position : new Cesium.Cartesian3(0.0, 0.0, 0.0),
-            text     : 'WBG'
-        });
-        labels.add({
-            position : new Cesium.Cartesian3(1000000.0, 0.0, 0.0),
-            text     : 'East'
-        });
-        labels.add({
-            position : new Cesium.Cartesian3(0.0, 1000000.0, 0.0),
-            text     : 'North'
-        });
-        labels.add({
-            position : new Cesium.Cartesian3(0.0, 0.0, 1000000.0),
-            text     : 'Above'
-        });
-        scene.getPrimitives().add(labels);
-    }
-    addLabelsInReferenceFrame(scene, ellipsoid);
 
     (function tick() {
         scene.render();
