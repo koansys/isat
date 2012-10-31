62b7d2dd1bbcd958ba7abbcb41384e24432fc9bd
Chris Shenton <chris@koansys.com>
2012-10-31 15:35:50 -0400

    Selectors now picklists; sat name display styled by JS

--- cesiumtry/index.js
+++ cesiumtry/index.js
@@ -5,6 +5,7 @@
     var scene = new Cesium.Scene(canvas);
     var primitives = scene.getPrimitives();
     var ellipsoid = Cesium.Ellipsoid.WGS84;
+    var cb = new Cesium.CentralBody(ellipsoid);
     var clock = new Cesium.Clock();
     var satrecs = [];           // populated from onclick file load
     var satnames = [];          // populated from onclick file load
@@ -27,19 +28,13 @@
     // Tile Providers
 
     var bing = new Cesium.BingMapsTileProvider({// fails to detect 404 due to no net :-(
-        server : 'dev.virtualearth.net',
-        mapStyle : Cesium.BingMapsStyle.AERIAL // AERIAL, AERIAL_WITH_LABELS, COLLINS_BART, ORDNANACE_SURVEY, ROAD
-        // Some versions of Safari support WebGL, but don't correctly implement
-        // cross-origin image loading, so we need to load Bing imagery using a proxy.
-        // This Proxyy.ashx doesn't exist, nor does Cesium.DefaultProxy('/proxy/') from docs
-        //proxy : Cesium.FeatureDetection.supportsCrossOriginImagery() ? undefined : new Cesium.DefaultProxy('/Proxy.ashx')
+        server : 'dev.virtualearth.net'         // default:  mapStyle:Cesium.BingMapStyle.AERIAL
     });
     var osm = new Cesium.OpenStreetMapTileProvider({
         url : 'http://tile.openstreetmap.org/'
     });
     var single = new Cesium.SingleTileProvider('Images/NE2_50M_SR_W_4096.jpg');
 
-    var cb = new Cesium.CentralBody(ellipsoid);
     // How do we tell if we can't get Bing, and substitute flat map with 'single'?
     cb.dayTileProvider      = bing; // single; // composite;// bing; // osm; // esri;
     cb.nightImageSource     = 'Images/land_ocean_ice_lights_2048.jpg';
@@ -123,7 +118,7 @@
 
     function displayPositions(sats) {
         // Display positions, velocities of current satellites
-        // Limit it to first 50 else browser becomes unusable.
+        // Limit number displayed else browser becomes unusable.
         // BUG: Velocity doesn't agree with isstracker.com's KMH; problem with Units?
         // BUG: Latitude is OK, Longitude doesn't agree with ISS Tracker, Height is sometimes NaN
         var position_table = document.getElementById('positions');
@@ -196,8 +191,8 @@
     viewByGeolocation(scene);
 
     ///////////////////////////////////////////////////////////////////////////
-    // Handle UI events
-
+    // Utilities
+    
     function xyzKmFixed(pt, fix) {
         // Return string formatted for xyz scaled to Km, with fixed precision.
         return '(' +
@@ -207,6 +202,8 @@
             ')';
     }
     
+    ///////////////////////////////////////////////////////////////////////////
+    // Handle UI events
 
     function satelliteHoverDisplay(scene, ellipsoid) {
         // When you hover over a satellite, show its name in a popup
@@ -218,12 +215,17 @@
                 var name;
                 var cart;
                 if (pickedObject) { // is the entire Billboard, not just a single satellite in it
-                    // TODO: accessing _index directly feels wrong, as does _actualPosition
+                    // TODO: accessing _index directly feels wrong
                     name = satnames[pickedObject._index];
                     sat_div.textContent = name;
-                    sat_div.style.left = movement.endPosition.x + 'px'; // seems a bit high from mouse
-                    sat_div.style.top  = movement.endPosition.y + 'px';
+                    sat_div.style.left = movement.endPosition.x + 'px';
+                    sat_div.style.top  = movement.endPosition.y + 'px'; // seems a bit high from mouse
                     sat_div.style.display = ''; // remove any 'none'
+                    // The following used to work in <style> section, but stopped; why?
+                    sat_div.style.position = 'absolute'; // vital to positioning near cursor
+                    sat_div.style.padding = '4px';
+                    sat_div.style.backgroundColor = '#909090';
+                    sat_div.style.color = 'black';
                 }
                 else {
                     sat_div.style.display = 'none';
@@ -235,47 +237,32 @@
     satelliteHoverDisplay(scene, ellipsoid);
 
     // Switch map/tile providers
-    var tileProvider = bing;
-    var tile_bing = document.getElementById('tile_bing');
-    var tile_osm = document.getElementById('tile_osm');
-    var tile_single = document.getElementById('tile_single');
-    tile_bing.onclick = function () {
-        cb.dayTileProvider      = bing;//bing; // composite;// bing; // osm; // esri; // single;
+    document.getElementById('select_tile_provider').onchange = function (event) {
+        var providers = {'bing' : bing,
+                         'osm'  : osm,
+                         'static' : single};
+        if (providers[this.value]) {
+            cb.dayTileProvider = providers[this.value];
+        }
     };
-    tile_osm.onclick = function () {
-        cb.dayTileProvider      = osm;//bing; // composite;// bing; // osm; // esri; // single;
-    };
-    tile_single.onclick = function () {
-        cb.dayTileProvider      = single;//bing; // composite;// bing; // osm; // esri; // single;
-    };
-
+    
     // Transition between views
     var transitioner = new Cesium.SceneTransitioner(scene);
-    var view_3d = document.getElementById('view_3d');
-    var view_2d = document.getElementById('view_2d');
-    view_2d.onclick = function () {
-        transitioner.morphTo2D();
-    };
-    view_3d.onclick = function () {
-        transitioner.morphTo3D();
+    document.getElementById('select_view').onchange = function (event) {
+        if (this.value === '2D') {
+            transitioner.morphTo2D();
+        }
+        else {
+            transitioner.morphTo3D();
+        }
     };
 
     // Switch which satellites are displayed.
-    document.getElementById('satellites_smd').onclick = function () {
-        getSatrecsFromTLEFile('tle/smd.txt');
+    document.getElementById('select_satellite_group').onchange = function (event) {
+        getSatrecsFromTLEFile('tle/' + this.value + '.txt'); // TODO: security risk?
     };
-    document.getElementById('satellites_iss').onclick = function () {
-        getSatrecsFromTLEFile('tle/iss.txt');
-    };
-    document.getElementById('satellites_stations').onclick = function () {
-        getSatrecsFromTLEFile('tle/space-stations.txt');
-    };
-    document.getElementById('satellites_science').onclick = function () {
-        getSatrecsFromTLEFile('tle/science.txt');
-    };
-    document.getElementById('satellites_geosynchronous').onclick = function () {
-        getSatrecsFromTLEFile('tle/geo.txt');
-    };
+    getSatrecsFromTLEFile('tle/' + document.getElementById('select_satellite_group').value + '.txt');
+
 
     /////////////////////////////////////////////////////////////////////////////
     // Run the timeclock, drive the animations
