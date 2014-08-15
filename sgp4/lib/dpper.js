f874d0858f25551976bdb9d625dcc6008ba801d7
Reed O'Brien <reed@reedobrien.com>
2014-08-15 12:36:25 -0400

    Update docs

--- sgp4/lib/dpper.js
+++ sgp4/lib/dpper.js
@@ -1,81 +1,39 @@
 "use strict";
-//                            procedure dpper
-//
-//   this procedure provides deep space long period periodic contributions
-//     to the mean elements.  by design, these periodics are zero at epoch.
-//     this used to be dscom which included initialization, but it's really a
-//     recurring function.
-//
-// Author:
-//   Jeff Beck
-//   beckja@alumni.lehigh.edu
-//   1.0 (aug 7, 2006) - update for paper dav
-// original comments from Vallado C++ version:
-//   author        : david vallado                  719-573-2600   28 jun 2005
-//
-//   inputs        :
-//     e3          -
-//     ee2         -
-//     peo         -
-//     pgho        -
-//     pho         -
-//     pinco       -
-//     plo         -
-//     se2 , se3 , Sgh2, Sgh3, Sgh4, Sh2, Sh3, Si2, Si3, Sl2, Sl3, Sl4 -
-//     t           -
-//     xh2, xh3, xi2, xi3, xl2, xl3, xl4 -
-//     zmol        -
-//     zmos        -
-//     ep          - eccentricity                           0.0 - 1.0
-//     inclo       - inclination - needed for lyddane modification
-//     nodep       - right ascension of ascending node
-//     argpp       - argument of perigee
-//     mp          - mean anomaly
-//
-//   outputs       :
-//     ep          - eccentricity                           0.0 - 1.0
-//     inclp       - inclination
-//     nodep       - right ascension of ascending node
-//     argpp       - argument of perigee
-//     mp          - mean anomaly
-//
-//   locals        :
-//     alfdp       -
-//     betdp       -
-//     cosip  , sinip  , cosop  , sinop  ,
-//     dalf        -
-//     dbet        -
-//     dls         -
-//     f2, f3      -
-//     pe          -
-//     pgh         -
-//     ph          -
-//     pinc        -
-//     pl          -
-//     sel   , ses   , sghl  , sghs  , shl   , shs   , sil   , sinzf , sis   ,
-//     sll   , sls
-//     xls         -
-//     xnoh        -
-//     zf          -
-//     zm          -
-//
-//   coupling      :
-//     none.
-//
-//   references    :
-//     hoots, roehrich, norad spacetrack report #3 1980
-//     hoots, norad spacetrack report #6 1986
-//     hoots, schumacher and glover 2004
-//     vallado, crawford, hujsak, kelso  2006
-//  ----------------------------------------------------------------------------*/
 
 define ([], function(){
 	var opsmode;
+	// dpper
+	// -----
+	// This procedure provides deep space long period periodic contributions
+	// to the mean elements.  by design, these periodics are zero at epoch.
+	// this used to be dscom which included initialization, but it's really a
+	// recurring function.
+	//
+	//     you dont want to see this methods signature:/
 	return function (e3,     ee2,    peo,    pgho,   pho,    pinco,  plo,    se2,
 					 se3,    sgh2,   sgh3,   sgh4,   sh2,    sh3,    si2,    si3,
 					 sl2,    sl3,    sl4,    t,      xgh2,   xgh3,   xgh4,   xh2,
 					 xh3,    xi2,    xi3,    xl2,    xl3,    xl4,    zmol,
 					 zmos,   inclo,  init,   ep,     inclp,  nodep, argpp,  mp) {
+					// inputs:
+					//
+					//     e3          -
+					//     ee2         -
+					//     peo         -
+					//     pgho        -
+					//     pho         -
+					//     pinco       -
+					//     plo         -
+					//     se2 , se3 , Sgh2, Sgh3, Sgh4, Sh2, Sh3, Si2, Si3, Sl2, Sl3, Sl4 -
+					//     t           -
+					//     xh2, xh3, xi2, xi3, xl2, xl3, xl4 -
+					//     zmol        -
+					//     zmos        -
+					//     ep          - eccentricity                           0.0 - 1.0
+					//     inclo       - inclination - needed for lyddane modification
+					//     nodep       - right ascension of ascending node
+					//     argpp       - argument of perigee
+					//     mp          - mean anomaly
 
 						 if (typeof(opsmode) === "undefined") {
 							 opsmode = "i";
@@ -83,9 +41,29 @@ define ([], function(){
 						 if ((opsmode !== "a") && (opsmode !== "i")) {
 							 throw new Error("opsmode must be \"a\" or \"i\" but opsmode=" + opsmode);
 						 }
+					// locals:
+					//
+					//     alfdp       -
+					//     betdp       -
+					//     cosip  , sinip  , cosop  , sinop  ,
+					//     dalf        -
+					//     dbet        -
+					//     dls         -
+					//     f2, f3      -
+					//     pe          -
+					//     pgh         -
+					//     ph          -
+					//     pinc        -
+					//     pl          -
+					//     sel   , ses   , sghl  , sghs  , shl   , shs   , sil   , sinzf , sis   ,
+					//     sll   , sls
+					//     xls         -
+					//     xnoh        -
+					//     zf          -
+					//     zm          -
 
 						 var twopi = 2.0 * Math.PI,
-						 // /* ---------------------- constants ----------------------------- */
+						 // ## constants
 						 zns   = 1.19459e-5,
 						 zes   = 0.01675,
 						 znl   = 1.5835218e-4,
@@ -96,8 +74,8 @@ define ([], function(){
 						 sinip, cosip,
 						 sinop, cosop, alfdp, dalf, dbet, betdp, xls, dls, xnoh;
 
-						 // /* --------------- calculate time varying periodics ----------- */
-						 // // be sure that the initial call has time set to zero
+						 // ## calculate time varying periodics
+						 // be sure that the initial call has time set to zero
 						 if (init === "y") {
 							 zm = zmos;
 						 }
@@ -143,13 +121,13 @@ define ([], function(){
 							 sinip = Math.sin(inclp);
 							 cosip = Math.cos(inclp);
 
-							 // /* ----------------- apply periodics directly ------------ */
+							 // ## apply periodics directly
 							 //  sgp4fix for lyddane choice
 							 //  strn3 used original inclination - this is technically feasible
 							 //  gsfc used perturbed inclination - also technically feasible
 							 //  probably best to readjust the 0.2 limit value and limit discontinuity
 							 //  use next line for original strn3 approach and original inclination
-							 //  if (inclo >= 0.2)
+							 //  `if (inclo >= 0.2)`
 							 //  use next line for gsfc version and perturbed inclination
 							 if (inclp >= 0.2) {
 								 ph     = ph / sinip;
@@ -159,7 +137,7 @@ define ([], function(){
 								 mp     = mp + pl;
 							 }
 							 else {
-								 // /* ---- apply periodics with lyddane modification ---- */
+								 // ## apply periodics with lyddane modification
 								 sinop  = Math.sin(nodep);
 								 cosop  = Math.cos(nodep);
 								 alfdp  = sinip * sinop;
@@ -197,6 +175,13 @@ define ([], function(){
 							 }
 						 } // if init == "n"
 
+						// outputs:
+						//
+						//     ep          - eccentricity                           0.0 - 1.0
+						//     inclp       - inclination
+						//     nodep       - right ascension of ascending node
+						//     argpp       - argument of perigee
+						//     mp          - mean anomaly
 						 return [ep, inclp, nodep, argpp,  mp];
 					 };
 });
